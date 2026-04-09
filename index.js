16976dcd9cc7b23b78b6fd9bb844becadb5af2ac
Evanio Tech <evanioshark@gmail.com>
2026-04-09 14:58:20 -0300

    fix: retirado tipo na lista

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 // vou usar o array para adicionar meus personagems e suas respectivas xps
 let herois = [
-    { nome :"pikachu",xp : 900 , tipo: "Elétrico"},
-    { nome :"Bullbasaur" ,xp : 900, tipo:"Grama"},
-    { nome :"squirtle",xp : 900 , tipo:"Água"},
-    { nome :"Charizard" ,xp : 900, tipo:"Fogo"},
-    { nome :"Gengar",xp : 900 , tipo:"Fantasma"},
-    { nome :"Goldengo" ,xp : 900, tipo:"Aço"},
-    { nome :"Dragonite",xp : 900 , tipo:"Dragão"},
-    { nome :"Gardevoir" ,xp : 900, tipo:"Fada"},
+    { nome :"pikachu",xp : 2500 },
+    { nome :"Bullbasaur" ,xp : 5500},
+    { nome :"squirtle",xp : 7600 },
+    { nome :"Charizard" ,xp : 8900},
+    { nome :"Gengar",xp : 9002 },
+    { nome :"Goldengo" ,xp : 1500},
+    { nome :"Dragonite",xp : 12000},
+    { nome :"Gardevoir" ,xp : 900},
 ]
 
+switch (herois) {
+    case 2500:
+        
+        break;
 
+    default:
+        break;
+}
+
+for (let index = 0; index < herois.length; index++) {
+
+    const element = herois[index].xp;
+    if(element < 1000) {
+        let nivel = "Ferro"
+        console.log(`O Herói de nome **${herois[index].nome}** está no nível de **${nivel}**`)
+    }
+    else if(element > 1000 && element <=2000) {
+        let nivel = "Bronze"
+        console.log(`O Herói de nome **${herois[index].nome}** está no nível de **${nivel}**`)
+    }
+    else if(element > 2000 && element <=5000) {
+        let nivel = "Prata"
+        console.log(`O Herói de nome **${herois[index].nome}** está no nível de **${nivel}**`)
+    }
+    else if(element > 5000 && element <=7000) {
+        let nivel = "Ouro"
+        console.log(`O Herói de nome **${herois[index].nome}** está no nível de **${nivel}**`)
+    }
+    else if(element > 7000 && element <= 8000) {
+        let nivel = "Platina"
+        console.log(`O Herói de nome **${herois[index].nome}** está no nível de **${nivel}**`)
+    }
+    else if(element > 8000 && element <= 9000) {
+        let nivel = "Ascendente"
+        console.log(`O Herói de nome **${herois[index].nome}** está no nível de **${nivel}**`)
+    }
+    else if(element > 9000 && element <= 10000) {
+        let nivel = "Imortal"
+        console.log(`O Herói de nome **${herois[index].nome}** está no nível de **${nivel}**`)
+    }
+    else if(element >= 10001) {
+        let nivel = "Radiante"
+        console.log(`O Herói de nome **${herois[index].nome}** está no nível de **${nivel}**`)
+    }
+    
+    
+    
+}
