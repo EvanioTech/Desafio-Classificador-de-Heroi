a33c4d62db356f14d0f8326925c4c1cc499d55ed
Evanio Tech <evanioshark@gmail.com>
2026-04-09 13:43:48 -0300

    fix: ajuste na lista de heróis

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // vou usar o array para adicionar meus personagems e suas respectivas xps
 let herois = [
-    ["pikachu", 900 , "Elétrico"],
-    ["Bullbasaur" , 900, "Grama"],
-    ["squirtle", 900 , "Água"],
-    ["Charizard" , 900, "Fogo"],
-    ["Gengar", 900 , "Fantasma"],
-    ["Goldengo" , 900, "Aço"],
-    ["Dragonite", 900 , "Dragão"],
-    ["Gardevoir" , 900, "Fada"],
+    { nome :"pikachu",xp : 900 , tipo: "Elétrico"},
+    { nome :"Bullbasaur" ,xp : 900, tipo:"Grama"},
+    { nome :"squirtle",xp : 900 , tipo:"Água"},
+    { nome :"Charizard" ,xp : 900, tipo:"Fogo"},
+    { nome :"Gengar",xp : 900 , tipo:"Fantasma"},
+    { nome :"Goldengo" ,xp : 900, tipo:"Aço"},
+    { nome :"Dragonite",xp : 900 , tipo:"Dragão"},
+    { nome :"Gardevoir" ,xp : 900, tipo:"Fada"},
 ]
+
 
