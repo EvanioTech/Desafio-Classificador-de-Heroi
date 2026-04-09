6ce55299b9f19c14a31c093757e958a4e3ff2686
Evanio Tech <evanioshark@gmail.com>
2026-04-09 13:23:11 -0300

    feat: Criado a lista de heróis

---
+++ index.js
@@ -0,0 +1,12 @@
+// vou usar o array para adicionar meus personagems e suas respectivas xps
+let herois = [
+    ["pikachu", 900 , "Elétrico"],
+    ["Bullbasaur" , 900, "Grama"],
+    ["squirtle", 900 , "Água"],
+    ["Charizard" , 900, "Fogo"],
+    ["Gengar", 900 , "Fantasma"],
+    ["Goldengo" , 900, "Aço"],
+    ["Dragonite", 900 , "Dragão"],
+    ["Gardevoir" , 900, "Fada"],
+]
+
